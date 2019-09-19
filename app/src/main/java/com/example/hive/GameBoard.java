a55ddc639fa1255a11da610ccc3345cc0d70baee
Phuocan Nguyen <phuocan.nguyen8@gmail.com>
2019-09-18 23:36:47 -0700

    Added two buttons along with a hexagon tile selection bar at the bottom

--- app/src/main/java/com/example/hive/GameBoard.java
+++ app/src/main/java/com/example/hive/GameBoard.java
@@ -34,6 +34,7 @@ public class GameBoard extends SurfaceView {
         paint.setColor(Color.WHITE);
         paint.setStrokeWidth(5.0f);
 
+        //Hexagons are drawn in mid game
         drawHexagon(canvas, 400, 400, 150);
         drawHexagon(canvas, 400 + 75 * 3^(1/2), 475, 150);
         drawHexagon(canvas, 400 + 75 * 3^(1/2), 625, 150);
@@ -45,6 +46,8 @@ public class GameBoard extends SurfaceView {
 
         paint.setTextSize(25);
 
+
+        //text written for pieces already on board
         canvas.drawText("White Queen Bee", 400, 475, paint);
         canvas.drawText("White Ant",670, 550, paint);
         canvas.drawText("White Spider", 625, 700, paint);
@@ -54,5 +57,16 @@ public class GameBoard extends SurfaceView {
         canvas.drawText("Black Ant",425, 925, paint);
         canvas.drawText("Black Spider", 850, 925, paint);
         canvas.drawText("Black Beetle", 850, 1075, paint);
+
+
+        //Bottom selection board
+        drawHexagon(canvas, 75, 1450, 150);
+        canvas.drawText("Queen Bee", 90, 1530, paint);
+        drawHexagon(canvas, 375, 1450, 150);
+        canvas.drawText("Ant", 420, 1530, paint);
+        drawHexagon(canvas, 675, 1450, 150);
+        canvas.drawText("Spider", 705, 1530, paint);
+        drawHexagon(canvas, 975, 1450, 150);
+        canvas.drawText("Beetle", 1005, 1530, paint);
     }
 }
