45e5d534d2ad3d205e89fa790579f2d6b466b26c
liuer21 <54870441+liuer21@users.noreply.github.com>
2019-09-19 18:14:56 -0700

    edited the drawHexagon function to draw in red to show possible moves

--- app/src/main/java/com/example/hive/GameBoard.java
+++ app/src/main/java/com/example/hive/GameBoard.java
@@ -13,6 +13,9 @@ public class GameBoard extends SurfaceView {
     Paint currentMove = new Paint();
     Paint paint = new Paint();
 
+    protected static int WHITE = 0;
+    protected static int RED = 1;
+
     public GameBoard(Context context, AttributeSet attrs) {
         super(context, attrs);
 
@@ -22,9 +25,13 @@ public class GameBoard extends SurfaceView {
         setWillNotDraw(false);
     }
 
-    public void drawHexagon(Canvas canvas, int startX, int startY, int size)
+    public void drawHexagon(Canvas canvas, int startX, int startY, int size, int color)
     {
-        paint.setColor(Color.WHITE);
+        if(color == WHITE)
+            paint.setColor(Color.WHITE);
+        else if(color == RED)
+            paint.setColor(Color.RED);
+
         canvas.drawLine(startX, startY, startX + size, startY, paint);
         canvas.drawLine(startX + size, startY, startX + (size/2) * 3^(1/2), startY + size/2, paint);
         canvas.drawLine(startX + (size/2)*3 ^(1/2), startY + size/2, startX + size, startY + size, paint);
@@ -44,21 +51,31 @@ public class GameBoard extends SurfaceView {
         paint.setStrokeWidth(5.0f);
 
         //Hexagons are drawn in mid game
-        drawHexagon(canvas, 400, 400, 150);
-        drawHexagon(canvas, 400 + 75 * 3^(1/2), 475, 150);
+        drawHexagon(canvas, 400, 400, 150, WHITE);
+        drawHexagon(canvas, 400 + 75 * 3^(1/2), 475, 150, WHITE);
 
-        drawHexagon(canvas, 400 + 75 * 3^(1/2), 625, 150);
+        drawHexagon(canvas, 400 + 75 * 3^(1/2), 625, 150, WHITE);
 
-        drawHexagon(canvas, 400 + 75 * 3^(1/2), 775, 150);
-        drawHexagon(canvas, 400 + 75 * 3^(1/2), 925, 150);
-        drawHexagon(canvas, 400, 850, 150);
-        drawHexagon(canvas, 700 + 50 * 3^(1/2), 850, 150);
-        drawHexagon(canvas, 700 + 50 * 3^(1/2), 1000, 150);
+        drawHexagon(canvas, 400 + 75 * 3^(1/2), 775, 150, WHITE);
+        drawHexagon(canvas, 400 + 75 * 3^(1/2), 925, 150, WHITE);
+        drawHexagon(canvas, 400, 850, 150, WHITE);
+        drawHexagon(canvas, 700 + 50 * 3^(1/2), 850, 150, WHITE);
+        drawHexagon(canvas, 700 + 50 * 3^(1/2), 1000, 150, WHITE);
 
-        paint.setTextSize(25);
+        //drawing hexagons for avaliable moves
 
+        drawHexagon(canvas, 400, 550, 150, RED);
+        drawHexagon(canvas, 400 - 75 * 3^(1/2), 475, 150, RED);
+        drawHexagon(canvas, 400 - 75 * 3^(1/2), 325, 150, RED);
+        drawHexagon(canvas, 400, 250, 150, RED);
+        drawHexagon(canvas, 400 + 75 * 3^(1/2), 325, 150, RED);
+        drawHexagon(canvas, 700 + 50 * 3^(1/2), 400, 150, RED);
+        drawHexagon(canvas, 700 + 50 * 3^(1/2), 550, 150, RED);
 
         //text written for pieces already on board
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(25);
+
         canvas.drawText("White Queen Bee", 400, 475, paint);
         canvas.drawText("White Ant",670, 550, paint);
         canvas.drawText("White Spider", 625, 700, paint);
@@ -70,16 +87,15 @@ public class GameBoard extends SurfaceView {
         canvas.drawText("Black Beetle", 850, 1075, paint);
 
 
-
         //Bottom selection board
 
-        drawHexagon(canvas, 75, 1450, 150);
+        drawHexagon(canvas, 75, 1450, 150, WHITE);
         canvas.drawText("Queen Bee", 90, 1530, paint);
-        drawHexagon(canvas, 375, 1450, 150);
+        drawHexagon(canvas, 375, 1450, 150, RED);
         canvas.drawText("Ant", 420, 1530, currentMove);
-        drawHexagon(canvas, 675, 1450, 150);
+        drawHexagon(canvas, 675, 1450, 150, WHITE);
         canvas.drawText("Spider", 705, 1530, paint);
-        drawHexagon(canvas, 975, 1450, 150);
+        drawHexagon(canvas, 975, 1450, 150, WHITE);
         canvas.drawText("Beetle", 1005, 1530, paint);
 
         //Who's turn it is
